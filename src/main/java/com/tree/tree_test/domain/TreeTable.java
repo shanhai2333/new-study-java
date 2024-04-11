a49c9db1615d3de7a22581f7c2349fcfaf8efb77
XieShangFeng <1195585382@qq.com>
2024-04-11 14:46:48 +0800

    菜单

--- src/main/java/com/tree/tree_test/domain/TreeTable.java
+++ src/main/java/com/tree/tree_test/domain/TreeTable.java
@@ -6,9 +6,11 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @TableName(value = "tree")
 public class TreeTable extends Model<TreeTable> {
