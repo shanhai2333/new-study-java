571f6f7c7de7d3ada11fdb845b59b310ef41e3ec
clearsky <clearsky@DESKTOP-MJ1D60C>
2024-04-11 21:02:53 +0800

    update

--- src/main/java/com/tree/tree_test/service/UpdateTreeService.java
+++ src/main/java/com/tree/tree_test/service/UpdateTreeService.java
@@ -1,8 +1,9 @@
 package com.tree.tree_test.service;
 
+import com.alibaba.fastjson2.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UpdateTreeService {
-    String UpdateTree();
+    String UpdateTree(Integer id , JSONObject jsonObject);
 }
--- src/main/java/com/tree/tree_test/service/impl/UpdateTreeTreeServiceImpl.java
+++ src/main/java/com/tree/tree_test/service/impl/UpdateTreeTreeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.tree.tree_test.service.impl;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.tree.tree_test.domain.TreeTable;
 import com.tree.tree_test.mapper.TreeTableMapper;
 import com.tree.tree_test.service.UpdateTreeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,9 @@ public class UpdateTreeTreeServiceImpl implements UpdateTreeService {
     @Autowired
     private TreeTableMapper treeTableMapper;
     @Override
-    public String UpdateTree() {
-
+    public String UpdateTree(Integer id , JSONObject jsonObject) {
+        TreeTable treeTable = new TreeTable().selectById(id);
+        treeTable.setDate(jsonObject);
         return null;
     }
 }
